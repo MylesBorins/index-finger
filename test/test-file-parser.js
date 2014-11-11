999a37f686a567065113d03fa5a2b98fde59ea6a
Myles Borins <myles@famo.us>
2014-11-11 15:36:39 -0800

    First pass at file parser

---
+++ test/test-file-parser.js
@@ -0,0 +1,27 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+'use strict';
+
+var path = require('path');
+
+var fileParser = require('../lib/file-parser');
+var test = require('tape');
+
+var fixturesPath = path.join(__dirname, 'fixtures', 'example');
+
+var expected = require('./fixtures/expected-file-parsed');
+
+test('file-parser: can load', function (t) {
+  t.plan(1);
+  t.ok(fileParser, 'it should be requireable');
+});
+
+test('file-parse: return', function (t) {
+  t.plan(2);
+  fileParser(fixturesPath, function (err, data) {
+    t.notok(err, 'it should return without an error');
+    t.deepEqual(data, expected, 'it should return the appropriate data');
+  });
+});
--- test/test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var test = require('tape');
-
-var lib = require('../lib');
-
-test('base test', function (t) {
-  t.plan(1);
-  t.equal(lib, 'module exports');
-});
