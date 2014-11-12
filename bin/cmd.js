035d4ce8051f7aafa2136e80245cd265fe8e5b6c
Myles Borins <myles@famo.us>
2014-11-12 13:46:34 -0800

    Added license header

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,6 +1,14 @@
 #!/usr/bin/env node
+
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 /*eslint no-process-exit:0*/
+
 'use strict';
+
 var argv = require('minimist')(process.argv.slice(2));
 var lib = require('../lib');
 
