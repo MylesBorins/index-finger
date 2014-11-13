947c553dc27221317c68bc8f90b925f435bf12e2
Myles Borins <myles@famo.us>
2014-11-12 16:11:49 -0800

    Finish stubbing out tests

--- test/test-lib.js
+++ test/test-lib.js
@@ -22,14 +22,14 @@ var outPath = path.join(tempdir, 'index-finger');
 var expectedIndexPath = path.join(__dirname, 'fixtures', 'lib', 'expected-index.js');
 var expectedIndex = fs.readFileSync(expectedIndexPath, 'utf8');
 
-// var expectedCorePath = path.join(__dirname, 'fixtures', 'lib', 'expected-core.js');
-// var expectedCore = fs.readFileSync(expectedCorePath, 'utf8');
-//
-// var expectedIndexPath = path.join(__dirname, 'fixtures', 'lib', 'expected-index.js');
-// var expectedIndex = fs.readFileSync(expectedIndexPath, 'utf8');
-//
-// var expectedIndexPath = path.join(__dirname, 'fixtures', 'lib', 'expected-index.js');
-// var expectedIndex = fs.readFileSync(expectedIndexPath, 'utf8');
+var expectedCorePath = path.join(__dirname, 'fixtures', 'lib', 'expected-core.js');
+var expectedCore = fs.readFileSync(expectedCorePath, 'utf8');
+
+var expectedPhysicsPath = path.join(__dirname, 'fixtures', 'lib', 'expected-physics.js');
+var expectedPhysics = fs.readFileSync(expectedPhysicsPath, 'utf8');
+
+var expectedForcesPath = path.join(__dirname, 'fixtures', 'lib', 'expected-forces.js');
+var expectedForces = fs.readFileSync(expectedForcesPath, 'utf8');
 
 test('index-finger: setup', function (t) {
   t.plan(2);
@@ -48,10 +48,10 @@ test('index-finger: works as expected', function (t) {
   t.plan(5);
   indexFinger(source, outPath, function (err) {
     t.notok(err, 'should exit without an error');
-    t.equal(fs.readFileSync(path.join(outPath, 'index.js'), 'utf8'), expectedIndex, 'the generated index should have the expected value');
-    t.ok(fs.existsSync(path.join(outPath, 'core', 'index.js')), 'there should be an index in the core directory');
-    t.ok(fs.existsSync(path.join(outPath, 'physics', 'index.js')), 'there should be an index in the physics directory');
-    t.ok(fs.existsSync(path.join(outPath, 'physics', 'forces', 'index.js')), 'there should be an index in the physics/forces directory');
+    t.equal(fs.readFileSync(path.join(outPath, 'index.js'), 'utf8'), expectedIndex, 'the generated index should have the expected content');
+    t.equal(fs.readFileSync(path.join(outPath, 'core', 'index.js'), 'utf8'), expectedCore, 'the generated index in the core directory should have the expected content');
+    t.equal(fs.readFileSync(path.join(outPath, 'physics', 'index.js'), 'utf8'), expectedPhysics, 'the generated index in the physics directory should have the expected content');
+    t.equal(fs.readFileSync(path.join(outPath, 'physics', 'forces', 'index.js'), 'utf8'), expectedForces, 'the generated index in the physics/forces directory should have the expected content');
   });
 });
 
